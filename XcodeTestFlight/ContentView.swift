c96ecaa3415909e0ed89d674067993501a8691ac
Alkın Çakıralar <a.cakiralar@gmail.com>
2023-02-24 11:25:41 +0300

    Initial Commit

---
+++ XcodeTestFlight/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  XcodeTestFlight
+//
+//  Created by Alkın Çakıralar on 24.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, worllkjnkhjd!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XcodeTestFlight/XcodeTestFlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeTestFlightApp.swift
+//  XcodeTestFlight
+//
+//  Created by Alkın Çakıralar on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct XcodeTestFlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
